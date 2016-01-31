9df60e209a0be8554fe156c72e1eafa71d43a233
Dustin Dinh <djdinh@gmail.com>
2016-01-30 19:25:13 -0800

    Finished IO

--- Dustin's Code/Christinas IO/FileGenerator/main.cpp
+++ Dustin's Code/Christinas IO/FileGenerator/main.cpp
@@ -32,7 +32,7 @@ void getinput(std::ifstream &inStream, std::vector<std::vector<int>> &returnCoin
 void formatLine(std::string &rawString);
 std::vector<int> convertToNumber(std::string &line);
 std::vector<std::vector<int>> getNumbers(std::ifstream &input);
-void writeResults(std::ofstream &output, std::vector<struct coins>& results, int total);
+void writeResults(std::ofstream &output, std::vector<int>& results, int total);
 void runtimetrial(int (*whichAlg)(std::vector<int>&, int, std::vector<int>&), std::vector<int> &coinSet, int coinSetValue, std::vector<int> &actualCoinsUsed);
 vector <struct coins> makeChange(int changeAmount, vector <struct coins> coinsUsedVector);
 int coinCounter(vector <struct coins> resultVector);
@@ -119,44 +119,25 @@ int main(int argc, char* argv[]){
     }
 	/* Display a babel for brute force algorithm time trial */
     std::cout << "Testing Bruteforce...." << std::endl;
-    for( unsigned int i = 0; i < coinsetinput.size(); i++ )
-    {
-        coinValues.clear();
-        resultVector.clear();
-        coinsUsedVector.clear();
-
-        changeAmount = changevalueV.at(i);
-        coinValues = coinsetinput.at(i);
-
-        //initialize
-        for (unsigned int i = 0; i < coinValues.size(); i++)
-        {
-            struct coins test;
-            test.coinAmount = 0;
-            test.coinValue = coinValues[i];
-            test.changeLeft = changeAmount;
-            coinsUsedVector.push_back(test);
-            // printf("pushed coin value %i\n", coinsUsedVector.back().coinValue);
-        }
-
-        std::clock_t start = std::clock();
     
-        resultVector = makeChange(changeAmount, coinsUsedVector);
-        resultCoins = coinCounter(resultVector);
-        double runtime = (std::clock() - start) / (double) CLOCKS_PER_SEC;
-
-        printf("Running time is %f\n", runtime);
-        printf("The minimum amount of coins used is: %i\n", resultCoins);
-
-        for (unsigned int i = 0; i < resultVector.size(); i++)
-        {
-            printf("Coin value: %i Amount: %i | ", resultVector[i].coinValue, resultVector[i].coinAmount);
+        //Question 4
+    //http://www.cplusplus.com/reference/vector/vector/vector/
+    // the iterator constructor can also be used to construct from arrays:
+    
+    std::vector<int> coinSet4;    
+    for(int i = 0; i <= 15; ++i ){
+        if(i == 0){
+            coinSet4.push_back(1);
+        }else{
+            coinSet4.push_back(i * 2);
         }
-        printf("\n");
-
-        timingStream << changeAmount << ", " << runtime << std::endl;
-        writeResults(textfile2, resultVector, resultCoins);	
     }
+
+    for(int i = 1; i <= 300; ++i ){
+        writeResults( textfile2, coinSet4, i );
+    }    
+
+ 
     timingStream.close();
     textfile2.close();	
 }
@@ -266,99 +247,17 @@ void getinput(std::ifstream &inStream, std::vector<std::vector<int>> &returnCoin
  * *  Function name: writeResults
  * *  Description: Function will write results to MSSResults text file
  * ***************************************************************/
-void writeResults(std::ofstream &output, std::vector<struct coins>& results, int total)
+void writeResults(std::ofstream &output, std::vector<int>& results,int total)
 {
     /* Use the << operator to write to a file and add back the bracket
 	CREDIT http://www.cplusplus.com/forum/beginner/4442/ */
 	output << "[";		
 	/* For each number, write it to the file followed by comma and space*/
         for( unsigned int i = 0; i < results.size() - 1; i++ )
-            output << results.at(i).coinAmount << ", ";
+            output << results.at(i) << ", ";
 	/* Once you get to last number, add a end bracket and newline */
-    output << results.at(results.size() - 1).coinAmount << "]" << std::endl;
+    output << results.at(results.size() - 1) << "]" << std::endl;
     /* Now we want to write the sum of max array from start to end on the file */
     output << total << "\n";
             
-}
-
-
-/**************************************************************
- * *  Function name: runtimetrial
- * *  Description: Function will write results to MSSResults text file
- * ***************************************************************/
-void runtimetrial(int (*whichAlg)(std::vector<int>&, int, std::vector<int>&), std::vector<int>& coinSet, int coinSetValue, std::vector<int> &actualCoinsUsed)
-{
-    std::clock_t start = std::clock();
-    int result = whichAlg( coinSet, coinSetValue, actualCoinsUsed );
-    double runtime = (std::clock() - start) / (double) CLOCKS_PER_SEC;
-    std::cout << "Coin Set Size(n): " << coinSet.size() << " Minimum Coins: " << result << " CPU Seconds: " << runtime << '\n';
-}
-
-vector <struct coins> makeChange(int changeAmount, vector <struct coins> coinsUsedVector)
-{
-    vector <struct coins> resultVector;
-    int resultCoins;
-    vector <struct coins> lowestCoinVector;
-    int lowestcoins = largeInt;
-    int newChangeAmount= 88;
-
-    vector <struct coins> currentVector;
-
-    //basecase
-    for (unsigned int i = 0; i < coinsUsedVector.size(); i++)
-    {
-        currentVector  = coinsUsedVector;
-
-        if (currentVector[i].coinValue == currentVector[0].changeLeft)
-        {
-            currentVector[i].coinAmount++;
-            currentVector[0].changeLeft -= currentVector[i].coinValue;
-            // printf("Base Case Reached Returning with +1 amount to coin value %i. Value: %i Amount: %i Value: %i Amount: %i\n",coinsUsedVector[i].coinValue, coinsUsedVector[0].coinValue, coinsUsedVector[0].coinAmount, coinsUsedVector[1].coinValue, coinsUsedVector[1].coinAmount );
-            return currentVector;
-        }
-    }
-
-    for (unsigned int i = 0; i < coinsUsedVector.size(); i++)
-    {
-        resultCoins = 0;
-        currentVector = coinsUsedVector;
-
-        // printf("Calling function with changeAmount %i - coin %i = %i \n", coinsUsedVector[0].changeLeft, currentVector[i].coinValue, currentVector[0].changeLeft);
-        if (coinsUsedVector[0].changeLeft > currentVector[i].coinValue)
-        {
-            currentVector[0].changeLeft = currentVector[0].changeLeft - currentVector[i].coinValue;
-            currentVector[i].coinAmount++;
-            resultVector = makeChange(newChangeAmount, currentVector);
-        }
-
-        
-        resultCoins = coinCounter(resultVector);
-        // printf("Comparing resultCoins %i to %i lowest coins\n", resultCoins, lowestcoins);
-        if (resultCoins < lowestcoins)
-        {
-            lowestcoins = resultCoins;
-            lowestCoinVector = resultVector;
-        }
-    }
-
-    resultCoins = coinCounter(lowestCoinVector);
-    // printf("The lowest coin vector is %i \n",  lowestcoins);
-
-    // printf("returning\n");
-    return lowestCoinVector;
-}
-
-int coinCounter(vector <struct coins> resultVector)
-{
-    int sumCoins = 0;
-
-    // printf("counting: ");
-    for (unsigned int i = 0; i < resultVector.size(); i++)
-    {
-        // printf("coin: %i count: %i ", resultVector[i].coinValue, resultVector[i].coinAmount );
-        sumCoins += resultVector[i].coinAmount;
-    }
-    // printf("\n");
-
-    return sumCoins;
 }
